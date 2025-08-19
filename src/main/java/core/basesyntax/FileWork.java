11d6fc5c8febdcaeab2b3a7293d423e12eaae4df
anton-chornobai <antonchornobajj1@gmail.com>
2025-08-19 18:31:04 +0200

    done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("[^a-zA-Z]+");
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        String lower = word.toLowerCase(Locale.ROOT);
+                        if (lower.charAt(0) == 'w') {
+                            lines.add(lower);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        Collections.sort(lines);
+        return lines.toArray(new String[0]);
     }
 }
--- src/test/java/core/basesyntax/FileWorkTest.java
+++ src/test/java/core/basesyntax/FileWorkTest.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
